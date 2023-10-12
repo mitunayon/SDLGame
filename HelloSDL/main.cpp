7f7372474a2d307f0ccceafeabe5d2ad1f467989
billy.karnchanapee <wkarnchanapee@gmail.com>
2023-10-12 20:55:11 +0100

    optimise images for screen format

--- HelloSDL/main.cpp
+++ HelloSDL/main.cpp
@@ -146,14 +146,29 @@ void close()
 
 SDL_Surface* loadSurface(std::string path)
 {
+	//The final optimized image
+	SDL_Surface* optimizedSurface = NULL;
+
 	//Load image at specified path
 	SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
 	if (loadedSurface == NULL)
 	{
 		printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
 	}
+	else
+	{
+		//Convert surface to screen format
+		optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+		if (optimizedSurface == NULL)
+		{
+			printf("Unable to optimise image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		}
 
-	return loadedSurface;
+		//Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	return optimizedSurface;
 }
 
 int main(int argc, char* args[])
