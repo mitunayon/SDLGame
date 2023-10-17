3b44279ab8ab15c304e020047ae9bf32286b5abc
billy.karnchanapee <wkarnchanapee@gmail.com>
2023-10-17 10:32:00 +0100

    complete 09 the viewport

--- HelloSDL/main.cpp
+++ HelloSDL/main.cpp
@@ -112,7 +112,7 @@ bool loadMedia()
 	bool success = true;
 	
 	//Load PNG texture
-	gTexture = loadTexture("07_texture_loading_and_rendering/texture.png");
+	gTexture = loadTexture("09_the_viewport/viewport.png");
 	if (gTexture == NULL)
 	{
 		printf("Failed to load texture image!\n");
@@ -186,30 +186,38 @@ int main(int argc, char* args[])
 						quit = true;
 					}
 
-					//Clear screen
-					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-					SDL_RenderClear(gRenderer);
+					//Top left corner viewport
+					SDL_Rect topLeftViewport;
+					topLeftViewport.x = 0;
+					topLeftViewport.y = 0;
+					topLeftViewport.w = SCREEN_WIDTH / 2;
+					topLeftViewport.h = SCREEN_HEIGHT / 2;
+					SDL_RenderSetViewport(gRenderer, &topLeftViewport);
 
-					//Render red filled quad. You can initialise a struct like by providing it's respective member variables like so
-					SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-					SDL_RenderFillRect(gRenderer, &fillRect);
+					//Render texture to screen
+					SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
-					//Render green outlined quad
-					SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
-					SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
-					SDL_RenderDrawRect(gRenderer, &outlineRect);
+					//Top right viewport
+					SDL_Rect topRightViewport;
+					topRightViewport.x = SCREEN_WIDTH / 2;
+					topRightViewport.y = 0;
+					topRightViewport.w = SCREEN_WIDTH / 2;
+					topRightViewport.h = SCREEN_HEIGHT / 2;
+					SDL_RenderSetViewport(gRenderer, &topRightViewport);
 
-					//Draw blue horizontal line
-					SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
-					SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+					//Render texture to screen
+					SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
-					//Draw vertical line of yellow dots
-					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
-					for (int i = 0; i < SCREEN_HEIGHT / 2; i += 4)
-					{
-						SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
-					}
+					//Bottom viewport
+					SDL_Rect bottomViewport;
+					bottomViewport.x = 0;
+					bottomViewport.y = SCREEN_HEIGHT / 2;
+					bottomViewport.w = SCREEN_WIDTH;
+					bottomViewport.h = SCREEN_HEIGHT / 2;
+					SDL_RenderSetViewport(gRenderer, &bottomViewport);
+
+					//Render texture to screen
+					SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
 					//Update screen
 					SDL_RenderPresent(gRenderer);
